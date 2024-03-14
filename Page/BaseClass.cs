0c947032caf894bb5aae65035f3e3ad78dd966f7
Lubna <lubnazarine@gmail.com>
2024-03-14 21:16:20 +0100

    added more args to options

--- Page/BaseClass.cs
+++ Page/BaseClass.cs
@@ -11,10 +11,14 @@ public class BaseClass
     public IWebDriver driver;
     public void BrowserSetUp()
     {
-        ChromeOptions option = new ChromeOptions();
-        option.AddArguments("--headless");
-        option.AddArguments("--remote-debugging-pipe");
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.AddArguments("start-maximized"); // open Browser in maximized mode
+        options.AddArguments("disable-infobars"); // disabling infobars
+        options.AddArguments("--disable-extensions"); // disabling extensions
+        options.AddArguments("--disable-gpu"); // applicable to windows os only
+        options.AddArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+        options.AddArguments("--no-sandbox"); // Bypass OS security model
+        driver = new ChromeDriver(options);
         string url = "https://www.flexera.com/flexera-one/business-value-calculator";
 
         driver.Manage().Window.Maximize();
